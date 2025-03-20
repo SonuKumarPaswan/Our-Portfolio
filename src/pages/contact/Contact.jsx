e3924776965c0b955e15ea2a4ecc00ccc3bf3aaa
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-03-20 20:32:51 +0530

    update

--- src/pages/contact/Contact.jsx
+++ src/pages/contact/Contact.jsx
@@ -5,6 +5,17 @@ const Contact = () => {
   return (
     <div>
       <h1>Contact me</h1>
+      <div></div>
+      <div>
+        <form action="">
+            <input type="text" placeholder='Enter the name' /> <br />
+            <input type="email" placeholder='Enter the Email' /> <br />
+            <input type="tel" placeholder='Enter the phone' /><br />
+            <input type="text" placeholder='Enter the name' /><br />
+            <textarea name="" id="" placeholder='Your message'></textarea> <br />
+            <button>Send</button>
+        </form>
+      </div>
     </div>
   )
 }
