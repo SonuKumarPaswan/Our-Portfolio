e2ca504c362da5f43ef29cf3de9da65bdee14529
Sonu Kumar Paswan <sonupaswan0381@gmail.com>
2025-03-26 16:43:27 +0530

    final commit

--- src/components/nav/Navbar.jsx
+++ src/components/nav/Navbar.jsx
@@ -10,12 +10,12 @@ const Navbar = () => {
          <h2><span>Port</span>folio</h2> 
         </div>
         <div className={style.links}>
-          <p><Link to={"/"} className={style.link}><span>Home</span></Link>  </p>
-          <p><Link to={"/about"} className={style.link}>About</Link>  </p>
-          <p><Link to={"/education"} className={style.link}>Education</Link>  </p>
-          <p><Link to={"/skills"} className={style.link}>Skills</Link>  </p>
-          <p><Link to={"/projects"}className={style.link}>Projects</Link>  </p>
-          <p><Link to={"/contact"}className={style.link}>Contact</Link>  </p>
+          <Link to={"/"} className={style.link}> <p><span>Home</span></p></Link>  
+          <Link to={"/about"} className={style.link}> <p>About</p> </Link> 
+          <Link to={"/education"} className={style.link}> <p>Education</p> </Link>  
+         <Link to={"/skills"} className={style.link}> <p>Skills</p> </Link> 
+          <Link to={"/projects"}className={style.link}>  <p>Projects</p>  </Link>
+          <Link to={"/contact"}className={style.link}> <p>Contact</p>  </Link>  
           {/* <button id={style.btn}><TiWeatherSunny  className={style.icon}/></button> */}
         </div>
     </div>
